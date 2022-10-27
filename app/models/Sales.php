d292ec5c48c6c2bc17020a0d4263839977f96ed8
marmol89 <38738460+marmol89@users.noreply.github.com>
2022-10-28 00:13:42 +0200

    Comentarios en Sales.php

--- app/models/Sales.php
+++ app/models/Sales.php
@@ -9,6 +9,9 @@ class Sales
         $this->db = Mysqldb::getInstance()->getDatabase();
     }
 
-
+    //Consulta de Prueba
+//use tiendamvc;
+//
+//select users.first_name , carts.date, carts.send , carts.discount, carts.quantity from carts INNER JOIN users on carts.user_id = users.id where users.id=1 AND carts.state=1;
 
 }
