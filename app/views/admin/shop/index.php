0a79954024f728cf30f7008d3ef2c4139282a454
marmol89 <38738460+marmol89@users.noreply.github.com>
2022-11-01 03:10:26 +0100

    Apartado visual Para AdminShop el index

--- app/views/admin/shop/index.php
+++ app/views/admin/shop/index.php
@@ -4,6 +4,23 @@
         <h1 class="text-center">Vista de Administración - Tienda</h1>
     </div>
     <div class="card-body">
+        <div class="row">
+        <div class="card text-bg-dark mb-1" style="max-width: 17rem;">
+            <div class="card-header">Bienvenido</div>
+            <div class="card-body">
+                <h5 class="card-title">Hola administrador</h5>
+                <p class="card-text">Si necesita algo pregunte con soporte</p>
+            </div>
+        </div>
+
+        <div class="card text-bg-warning mb-1 ms-3" style="max-width: 17rem;">
+            <div class="card-header">Funcionalidad Nueva</div>
+            <div class="card-body">
+                <h5 class="card-title">Warning en Ventas</h5>
+                <p class="card-text">Nueva apartado ventas si hay errores abisar a soporte para poder arreglarlo rapido</p>
+            </div>
+        </div>
+    </div>
 
     </div>
     <div class="card-footer">
