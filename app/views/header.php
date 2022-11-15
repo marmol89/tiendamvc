5962282cdcf7be110fa18dce4e21cb606f011182
marmol89 <38738460+marmol89@users.noreply.github.com>
2022-11-15 01:25:23 +0100

    Iconos bustrap

--- app/views/header.php
+++ app/views/header.php
@@ -15,6 +15,9 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
+    <!-- Iconos Bustrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+
     <!-- Font-Awesome -->
     <script src="https://kit.fontawesome.com/c858fc57f5.js" crossorigin="anonymous"></script>
 
