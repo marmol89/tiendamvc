4aeb5dfccbba1a4c50812465fbdb17432b79c017
marmol89 <josito_marmol@hotmail.com>
2022-11-17 19:43:10 +0100

    Arreglado Error Controler

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -4,15 +4,12 @@ class ErrorController extends Controller
 {
     public function index() {
 
-        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
         $urlST= trim($_SERVER['REQUEST_URI'], '/');
         $urlST = filter_var($urlST, FILTER_SANITIZE_URL);
         $urlST = explode('/', $urlST);
 
 
         $data = [
-            'url' => $url,
             'back' => $urlST[0],
         ];
 
